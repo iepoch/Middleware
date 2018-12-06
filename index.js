3f0e8ceb6af98e32f742d9509bfbd2dc1d21b8a7
iepoch <staclem@live.com>
2018-12-05 20:56:37 -0700

    fixed port issue

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //Create express server
 const express = require("express");
-const PORT = 9000;
 const helmet = require("helmet");
 const logger = require("morgan");
 const customMW = require("./custom_middleware.js");
 //add server
+const PORT = process.env.PORT || 9000;
 const server = express();
 
 //middleware
-server.use(express.json(), logger("tiny"), helmet(), customMW.gatekeeper)
+server.use(express.json(), logger("tiny"), helmet(), customMW.gatekeeper);
 
 // route handlers
 server.get("/", (req, res) => {
